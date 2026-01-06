2c45d2f77801d143dbf9d1d7d131bbc37671b88f
Chalana001 <ckalhara7277@gmail.com>
2026-01-06 17:44:24 +0530

    changed to browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-} from "react-router-dom";
+// import {
+//   HashRouter as Router,
+//   Routes,
+//   Route,
+//   useNavigate,
+// } from "react-router-dom";
+import { Router } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import MenuPage from "./pages/MenuPage";
